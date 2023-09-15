dec840ef67bf36a87ce9b3512e61cd683a781d19
damilolajohn6 <damilolajohn622@gmail.com>
2023-09-15 16:57:45 +0100

    updates routes for sign up users

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,23 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // User registration
 router.post("/register", async (req, res) => {
   try {
-    const { username, password } = req.body;
-    const user = new User({ username, password });
+    const { username, password, email, full_name, date_of_birth, address, phone_number } = req.body;
+    const user = new User({
+      username,
+      password,
+      email,
+      full_name,
+      date_of_birth,
+      address,
+      phone_number,
+    });
     await user.save();
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
@@ -23,7 +34,7 @@ router.post("/login", async (req, res) => {
     if (!user || user.password !== password) {
       throw new Error("Invalid login credentials");
     }
-    const token = jwt.sign({ userId: user._id }, "secret_key");
+    const token = jwt.sign({ userId: user._id }, "JWT_SECRET");
     res.json({ token });
   } catch (error) {
     res.status(401).json({ error: error.message });
