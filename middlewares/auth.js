e444e0576c44ee2593db2fe075da2489dbb24644
damilolajohn6 <damilolajohn622@gmail.com>
2023-09-15 10:13:13 +0100

    auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,25 @@
 const jwt = require("jsonwebtoken");
 
+const JWT_SECRET = process.env.JWT_SECRET;
+
 module.exports = (req, res, next) => {
   const token = req.header("Authorization");
 
   if (!token) {
-    return res.status(401).json({ error: "Unauthorized" });
+    return res.status(401).json({ error: "Authorization header missing" });
   }
 
   try {
-    const decoded = jwt.verify(token, "secret_key");
+    const decoded = jwt.verify(token, JWT_SECRET);
+
+    // Verify token expiry
+    if (Date.now() >= decoded.exp * 1000) {
+      return res.status(401).json({ error: "Token has expired" });
+    }
+
     req.userId = decoded.userId;
     next();
   } catch (error) {
-    res.status(401).json({ error: "Invalid token" });
+    return res.status(401).json({ error: "Invalid token" });
   }
 };
